67d84267b3e67743897caf1541713afd9378ef16
techy-ritik <ritikeshjee@gmail.com>
2026-03-20 18:01:34 +0530

    changed .use to .post for limiting the usage of the middleware based on the incoming method type request handling

--- rvsn/rePracticeCodes/expressFrameWork/xprsApp.js
+++ rvsn/rePracticeCodes/expressFrameWork/xprsApp.js
@@ -17,10 +17,10 @@ app.use("/", (req, res, next) => {
 
 app.use("/add-product", (req, res, next) => {
   console.log("in the add product middleware"); 
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+  res.send('<form action="/product" method="POST">product name:- <input type="text" name="title"> Product Size:-<input type="number" name="size"> <button type="submit">add product</button></form>');
 });
 
-app.use("/product",(req,res,next)=>{
+app.post("/product",(req,res,next)=>{  // we can use .get or .post in place of .use as for which method incoming request we are using this middleware and route. (Here .post is used bcz incoming request is post method of the form)
     console.log(req.body); // data sent back by the server which we Post on the server can we recived through req.body
     res.redirect("/");
 })
