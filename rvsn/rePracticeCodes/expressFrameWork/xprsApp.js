adba73402b07b1e3636ebe38f9305208b01c44f0
techy-ritik <ritikeshjee@gmail.com>
2026-03-20 11:10:28 +0530

    different routes handling middleWare is created and incoming request data parsing functionlity is added

--- rvsn/rePracticeCodes/expressFrameWork/xprsApp.js
+++ rvsn/rePracticeCodes/expressFrameWork/xprsApp.js
@@ -1,18 +1,35 @@
 console.log("Jay Hanuman")
-
 const express = require('express');
+const bodyParser = require('body-parser'); // this package is use to parse the data recieved through the req.body from the server
 
-const app = express();  // app is  basically a seerver object which handle requests and runs the server
+const app = express();  // app is  basically a server object which handle requests and runs the server
 
-app.use((req,res,next)=>{
-    console.log("in the middleware");
-    next(); // this method is used to accept the request from the next middleware in line
+app.use(bodyParser.urlencoded({extended:false}));  // It is used to parse recieved data into usable object through req.body
+                                                    // urlencoded means data is parsed as such it can be used to send in url as querry parameters
+                                                    // extended : false is used here to keep the parsing format simple and if it change to true the parsing would become advanced and the result output data format got changed
+
+
+app.use("/", (req, res, next) => {
+  console.log("in the 1st middleware");
+//   console.log("URL:", req.url, "Method:", req.method);
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in the add product middleware"); 
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+});
+
+app.use("/product",(req,res,next)=>{
+    console.log(req.body); // data sent back by the server which we Post on the server can we recived through req.body
+    res.redirect("/");
 })
 
-app.use((req,res,next)=>{
-    console.log("in the 2nd middleware");
-    res.send('<h1>Jay Hanuman ji</h1>')  // send default html responses to the server without manually setting the content type Header
-})
+app.use("/", (req, res, next) => {
+  console.log("in the 3rd middleware");
+//   console.log("URL:", req.url, "Method:", req.method);
+  res.send("<h1>Jay Hanuman Gyan Gun sagar</h1>"); // send default html responses to the server without manually setting the content type Header
+});
 
 
-app.listen(3000);
+app.listen(3000);  // it creates and also starts the server
