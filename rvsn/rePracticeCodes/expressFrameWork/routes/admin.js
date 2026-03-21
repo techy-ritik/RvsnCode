9111102f8908a7db677c750713648e844cbafd03
techy-ritik <ritikeshjee@gmail.com>
2026-03-21 08:20:17 +0530

    404 status code warning and router filtering functionality for same routes in different router implemented

--- rvsn/rePracticeCodes/expressFrameWork/routes/admin.js
+++ rvsn/rePracticeCodes/expressFrameWork/routes/admin.js
@@ -5,14 +5,15 @@ const router = express.Router();
 router.get("/add-product", (req, res, next) => { 
   console.log("in the add product middleware");
   res.send(
-    '<form action="/product" method="POST">product name:- <input type="text" name="title"> Product Size:-<input type="number" name="size"> <button type="submit">add product</button></form>',
+    '<form action="/admin/add-product" method="POST">product name:- <input type="text" name="title"> Product Size:-<input type="number" name="size"> <button type="submit">add product</button></form>',
   );
 });
 
-router.post("/product", (req, res, next) => {// we can use .get or .post in place of .use as for which method incoming request we are using this middleware and route. (Here .post is used bcz incoming request is post method of the form)
+router.post("/add-product", (req, res, next) => {// we can use .get or .post in place of .use as for which method incoming request we are using this middleware and route. (Here .post is used bcz incoming request is post method of the form)
   console.log(req.body); // data sent back by the server which we Post on the server can we recived through req.body
-  res.redirect("/");
+  res.redirect("/shop");
 });
 
+//here we using same route name in both of the above middleware but the method is different so it's executing separately at different method call
 
 module.exports = router;
