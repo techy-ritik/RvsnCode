63ce1045175dcaca7e6010e154aa70defb00b37c
techy-ritik <ritikeshjee@gmail.com>
2026-03-22 08:56:03 +0530

    username saved to localStorage

--- rvsn/projects/simpleGroupChatApplication/routes/admin.js
+++ rvsn/projects/simpleGroupChatApplication/routes/admin.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/login',(req,res)=>{
-    res.send('<form action="/" method="POST">Enter username:- <input type="text" name="login"><button>login</button></form>')
-    
-})
+router.get("/login", (req, res) => {
+  res.send(
+    '<form  action="/" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>',
+  );
+});
 
-router.post("/", (req, res) => {
+router.use("/", (req, res) => {
+  // console.log(req.body);
+
   res.send(
     '<form action="/" method="POST">Send message:- <input type="text" name="message"><button>send</button></form>',
   );
 });
 
-module.exports=router;
+module.exports = router;
