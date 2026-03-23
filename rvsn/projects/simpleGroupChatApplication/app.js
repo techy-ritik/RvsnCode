521845422410512f1f0f35dd600b3b5e8eab6f1b
techy-ritik <ritikeshjee@gmail.com>
2026-03-23 18:05:51 +0530

    username extarcted from localStorage on for multiple user -> using onsubmit from the send message form, functionality of chat with username added and readFile to display chat on screen impliminted

--- rvsn/projects/simpleGroupChatApplication/app.js
+++ rvsn/projects/simpleGroupChatApplication/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
 const adminRoute = require('./routes/admin')
 
 app.use(adminRoute);
--- rvsn/projects/simpleGroupChatApplication/routes/admin.js
+++ rvsn/projects/simpleGroupChatApplication/routes/admin.js
@@ -1,19 +1,41 @@
 const express = require("express");
+const fs = require("fs");
 
 const router = express.Router();
 
 router.get("/login", (req, res) => {
   res.send(
-    '<form  action="/" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>',
+    '<form  action="/login" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>',
   );
 });
 
-router.use("/", (req, res) => {
-  // console.log(req.body);
+router.post("/login", (req, res) => {
+  console.log(".post of /login", req.body);
 
-  res.send(
-    '<form action="/" method="POST">Send message:- <input type="text" name="message"><button>send</button></form>',
-  );
+  res.redirect("/");
+});
+
+
+router.get("/", (req, res) => {
+  console.log(".get of /", req.body);
+
+  fs.readFile("chatData", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(
+      `<p>${data}</p><form action="/" method="POST" onsubmit="this.currentUser.value=localStorage.getItem('Username')"><input type="hidden" name="currentUser">Send message:- <input type="text" name="message"><button>send</button></form>`,
+    );
+  });
+});
+
+router.post("/", (req, res) => {
+  console.log(".post of /", req.body);
+  const storedChat = Object.values(req.body).join(": ");  //.join() is an array method which helps to store extracted object values which was in the form array to in the form of string and joins each values together
+  fs.appendFile("chatData", storedChat + "\n", () => {
+    // .appendFile is used in place of writeFile so that new chat get added in the file without removing the previous chat
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
