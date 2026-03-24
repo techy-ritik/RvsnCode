b63e6b559e70ee4d20ccaf68966d0eddc6c26f4d
techy-ritik <ritikeshjee@gmail.com>
2026-03-24 09:05:10 +0530

    login alert and no chat warning for blank file added and with this the project completed

--- rvsn/projects/simpleGroupChatApplication/routes/admin.js
+++ rvsn/projects/simpleGroupChatApplication/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/login", (req, res) => {
   res.send(
-    '<form  action="/login" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>',
+    '<form  action="/login" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>', 
   );
 });
 
@@ -15,7 +15,6 @@ router.post("/login", (req, res) => {
   res.redirect("/");
 });
 
-
 router.get("/", (req, res) => {
   console.log(".get of /", req.body);
 
@@ -23,16 +22,34 @@ router.get("/", (req, res) => {
     if (err) {
       console.log(err);
     }
+    let chatData = "no chat yet!!"
+    if(data){
+      chatData=data;
+    }
+
     res.send(
-      `<p>${data}</p><form action="/" method="POST" onsubmit="this.currentUser.value=localStorage.getItem('Username')"><input type="hidden" name="currentUser">Send message:- <input type="text" name="message"><button>send</button></form>`,
+      `<p>${chatData}</p>
+      <form action="/" method="POST" 
+        onsubmit="const user = localStorage.getItem('Username');
+          if(!user){
+            alert('Please login');
+            return false;
+          }
+          this.currentUser.value= user "
+      >
+      <input type="hidden" name="currentUser">
+      Send message:- <input type="text" name="message">
+      <button>send</button>
+      </form>`, // here we have to use this keyword because userName is not global variable and without using this keyword it tries to find it in global scope which will not be found so to point on the form scope we have to use this keyword
+                // here only single quotes should be used for strings in between onsubmit function statement else it doesn't work
     );
   });
 });
 
 router.post("/", (req, res) => {
   console.log(".post of /", req.body);
-  const storedChat = Object.values(req.body).join(": ");  //.join() is an array method which helps to store extracted object values which was in the form array to in the form of string and joins each values together
-  fs.appendFile("chatData", storedChat + "\n", () => {
+  const storedChat = Object.values(req.body).join(": "); //.join() is an array method which helps to store extracted object values which was in the form array to in the form of string and joins each values together
+  fs.appendFile("chatData", "| " + storedChat + "\n", () => {
     // .appendFile is used in place of writeFile so that new chat get added in the file without removing the previous chat
   });
   res.redirect("/");
