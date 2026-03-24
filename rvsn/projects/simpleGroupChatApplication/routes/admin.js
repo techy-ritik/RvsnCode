58403831393d33372472e06d91304bdd5d1b0f79
techy-ritik <ritikeshjee@gmail.com>
2026-03-24 17:20:29 +0530

    some minor cleanups , project completed

--- rvsn/projects/simpleGroupChatApplication/routes/admin.js
+++ rvsn/projects/simpleGroupChatApplication/routes/admin.js
@@ -5,26 +5,26 @@ const router = express.Router();
 
 router.get("/login", (req, res) => {
   res.send(
-    '<form  action="/login" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>', 
+    '<form  action="/login" method="POST" onsubmit="localStorage.setItem(`Username`,document.getElementById(`username`).value)">Enter username:- <input id="username" type="text" name="userName"><button type="submit">login</button></form>',
   );
 });
 
 router.post("/login", (req, res) => {
-  console.log(".post of /login", req.body);
+  // console.log(".post of /login", req.body);
 
   res.redirect("/");
 });
 
 router.get("/", (req, res) => {
-  console.log(".get of /", req.body);
+  // console.log(".get of /", req.body);
 
   fs.readFile("chatData", "utf8", (err, data) => {
     if (err) {
       console.log(err);
     }
-    let chatData = "no chat yet!!"
-    if(data){
-      chatData=data;
+    let chatData = "no chat yet!!";
+    if (data) {
+      chatData = data;
     }
 
     res.send(
@@ -41,18 +41,18 @@ router.get("/", (req, res) => {
       Send message:- <input type="text" name="message">
       <button>send</button>
       </form>`, // here we have to use this keyword because userName is not global variable and without using this keyword it tries to find it in global scope which will not be found so to point on the form scope we have to use this keyword
-                // here only single quotes should be used for strings in between onsubmit function statement else it doesn't work
+      // here only single quotes should be used for strings in between onsubmit function statement else it doesn't work
     );
   });
 });
 
 router.post("/", (req, res) => {
-  console.log(".post of /", req.body);
+  // console.log(".post of /", req.body);
   const storedChat = Object.values(req.body).join(": "); //.join() is an array method which helps to store extracted object values which was in the form array to in the form of string and joins each values together
   fs.appendFile("chatData", "| " + storedChat + "\n", () => {
     // .appendFile is used in place of writeFile so that new chat get added in the file without removing the previous chat
+    res.redirect("/");
   });
-  res.redirect("/");
 });
 
 module.exports = router;
