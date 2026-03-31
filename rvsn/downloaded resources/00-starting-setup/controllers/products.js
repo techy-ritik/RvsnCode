6570f017e432aff2f016e327e1230fe0d2e5f86d
techy-ritik <ritikeshjee@gmail.com>
2026-03-31 07:53:45 +0530

    Controllers added and other files modified accordingly in downloaded resource

---
+++ rvsn/downloaded resources/00-starting-setup/controllers/products.js
@@ -0,0 +1,28 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+//   const products = adminData.products;     // not required to use products array through admin.products by import methd after moving this middleware in the controller where products array decalaration is already present
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
