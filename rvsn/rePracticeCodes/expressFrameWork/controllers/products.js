8e7e37c9a2cb8731588ee5598cde393b12e05aa1
techy-ritik <ritikeshjee@gmail.com>
2026-04-02 08:34:21 +0530

    added models product file with data getting stored in the array temperorily and middleware in the contoller modified accordingly

--- rvsn/rePracticeCodes/expressFrameWork/controllers/products.js
+++ rvsn/rePracticeCodes/expressFrameWork/controllers/products.js
@@ -1,6 +1,7 @@
 const path = require("path");
-
 const rootDir = require("../util/path");
+
+const productModel = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
   console.log("in the add product middleware");
@@ -10,10 +11,16 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   // we can use .get or .post in place of .use as for which method incoming request we are using this middleware and route. (Here .post is used bcz incoming request is post method of the form)
   console.log(req.body); // data sent back by the server which we Post on the server can we recived through req.body
+  
+  const newProduct = new productModel(req.body.title)  // here title is the name attribute which we set in the form input as we know the input value get stored as the (set name sttribute = the input value).
+  newProduct.save();
   res.redirect("/");
 };
 
 exports.getProductShop = (req, res, next) => {
   // res.sendFile(path.join(__dirname,'..','views','shop.html'))  //we can use '../' or '..' to go to one level upper folder
+  const storedProducts=productModel.fetchAll()
+  console.log(storedProducts);
+  // console.log(storedProducts[0].title);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 };
