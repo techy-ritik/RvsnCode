ccc37a56cfde15bec95a62f0d1eaeda4a4310544
techy-ritik <ritikeshjee@gmail.com>
2026-04-12 07:51:38 +0530

    callback dataFlow explanation added

--- rvsn/rePracticeCodes/expressFrameWork/models/product.js
+++ rvsn/rePracticeCodes/expressFrameWork/models/product.js
@@ -12,7 +12,7 @@ const readFileData = (callback) => {
     }
     let productList = JSON.parse(data); // we need to parse the data into js which we retrieve from the file to use as normal js code
     console.log("productList", productList);
-    callback(productList);
+    callback(productList);  // this callback send all product data through callback wherever readFileData() is called
   });
 }; // this function can also be created inside class but as the fetchAll() is ststic method and save() is normal so we would have been setting more logic to call in both place so it's more conveneient to define it here outside class
 
@@ -39,6 +39,7 @@ module.exports = class Product {
   static fetchAll(cb) {
     // static keyword is used here which assures that the method is called on the "products" class and don't touches the object
 
-    readFileData(cb);
+    readFileData(cb); // ** dataFlow of callbacks :- as cb is actually an argument which carry the function which is passed in fetchAll when we call it anywhere, and then that function is transfered in readFileData() when we pass same cb in it and then that function is sent as callback by passing argument where readFileData() is defined so whatever readFileData() is returning it will be available for any function to use which is passed in the readFileData()
+  
   } // here callback is passed because as we know js codes works on sync manner and doesn't wait for async codes to execute further so keep that in pending for later execution and the fs.readFile() is async proerty so when we returning the data from the readFile directly then it execute after the fetchAll() execution already get completed and wherever we call fetchAll() method then it shows undefined so to resolve this, we use callback as when we pass callback function, then while execution pointer get on fs.readFile() and finds the callback in argument then it keeps it in the memory and execute it by calling that cb function where it is defined so whatever we pass in the cb will be availabble for that function to use even when fetchAll() finished earlier
 }; //  here actually class is created so that we can define required customized methods and which can we then use at different places
