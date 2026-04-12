d49cfa54e9de2dd9a2d0a128f7aec2cb0f360e3b
techy-ritik <ritikeshjee@gmail.com>
2026-04-12 07:53:14 +0530

    functionality for fetching single product is added with explanation

--- rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/controllers/shop.js
+++ rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/controllers/shop.js
@@ -11,8 +11,11 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req,res,next)=>{
-  const prodId = req.params.productId;  // we use req.params to extract values of the dynamic part available in the url and the productId is the variable we set which we can use in the route file for adding dynamic value in the url and it is attached with colon
+  const prodId = req.params.productId;  // we use req.params to extract values from the dynamic part available in the url and the productId is the variable to be set same as which is used in the route so that we can extract it's value
   console.log("prodId", prodId);
+  Product.findById(prodId,(foundProduct)=>{   // here id is sent back to findById by passing prodId and product data received through callback function which is defined here and got stored in the foundProduct
+    console.log("foundProduct", foundProduct);
+  })
   res.redirect('/');
 }
 
--- rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/models/product.js
+++ rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/models/product.js
@@ -27,9 +27,9 @@ module.exports = class Product {
 
   save() {
     this.id = Math.random().toString();
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -38,4 +38,17 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
+  static findById(id, cb) {
+    // here we are extracting single product to show it's details, by using id value which is recieved from controller where findById method is called
+    getProductsFromFile((products) => {  // the data returned from getProductsFromFile() is available in the function which is passed here as callback and it's stored in products argument of this callback function
+                                            // so it means products argument contain list of all the products
+
+      const currentFoundProduct = products.find((p) => p.id === id);   // here .find() is inbuilt method of js which traverse over the array internally and each index element get temp. stored in, by creating new variable with each loop which is to be passed as argument in .find() method
+                                                                        //  so, as here the elements of the products array is in object form, .find() traverse through array and check for id of each element object through 'p.id' by matching it with the id which is recieved as argument, when it's passed while calling findById() method 
+      cb(currentFoundProduct);  // here as the cb is actually the callback function which is passed here as argument so whatever data is sent from here will be available there wherver the callback function is defined
+                                //  and here it's sending the data of one product whose id is matched with the recieved id
+    });  // overall -> ('id' receives data from where findById() is called and 'cb' sends data BACK to where findById() is called
+
+  } // here at first all the product has been fetched by calling getProductsFromFile() and then single product has been selected with the help of product id whose details are to be sent for the user display and then passed that with callback->'cb' 
 };
