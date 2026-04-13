1de4aaae229e735c4e6ec74ed33a5017282a8e89
techy-ritik <ritikeshjee@gmail.com>
2026-04-13 07:21:26 +0530

    details page code added and page rendered in controller

--- rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/controllers/shop.js
+++ rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/controllers/shop.js
@@ -1,51 +1,56 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
       prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+      pageTitle: "All Products",
+      path: "/products",
     });
   });
 };
 
-exports.getProduct = (req,res,next)=>{
-  const prodId = req.params.productId;  // we use req.params to extract values from the dynamic part available in the url and the productId is the variable to be set same as which is used in the route so that we can extract it's value
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId; // we use req.params to extract values from the dynamic part available in the url and the productId is the variable to be set same as which is used in the route so that we can extract it's value
   console.log("prodId", prodId);
-  Product.findById(prodId,(foundProduct)=>{   // here id is sent back to findById by passing prodId and product data received through callback function which is defined here and got stored in the foundProduct
+  Product.findById(prodId, (foundProduct) => {
+    // here id is sent back to findById by passing prodId and product data received through callback function which is defined here and got stored in the foundProduct
     console.log("foundProduct", foundProduct);
-  })
-  res.redirect('/');
-}
+    res.render("shop/product-detail", {  // as res.render specially desgined for ejs files so when we call it we don't need to enter full path, as it always look into views folder so we just need to enter folder and file name inside of views
+      product: foundProduct, // here product is the key which is set so that it can be accessed in the view
+      pageTitle: foundProduct.title,
+      path: "/products",   // sending path variable like this for active navigation links works only with ejs.
+    }); 
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+      pageTitle: "Shop",
+      path: "/",
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
   });
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
