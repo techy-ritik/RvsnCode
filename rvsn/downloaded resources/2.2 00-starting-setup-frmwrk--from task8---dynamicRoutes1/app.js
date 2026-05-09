6e1028808a1ee992edbff024567ded3bb7e99560
techy-ritik <ritikeshjee@gmail.com>
2026-05-09 06:58:25 +0530

    sequelize.sync added in app.js for executing model for implementing table creation and availability check

--- rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/app.js
+++ rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database')
 
 const app = express();
 
@@ -21,4 +22,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()       //  here when we use sequelize library with node.js then for creating table or for executing models where databse table is created, we have to run sequelize.sync inside app.js and also starts the server in then() as we know once we get the promise response of database creation or availability then only we can handle any data else there is now use of backend program
+.then((res)=>{
+    // console.log(res);
+    app.listen(3000);   // we have added in then() of sync bcz we want to start the server once it get confirmed that the database is created and avaialable to handle data
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
