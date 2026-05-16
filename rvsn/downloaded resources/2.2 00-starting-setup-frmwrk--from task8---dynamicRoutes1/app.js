405f20597be14e47f79d32a34ecf1d163096711f
techy-ritik <ritikeshjee@gmail.com>
2026-05-16 18:01:58 +0530

    dummy user added in the user table through sequelize.sync() and a middleware registered in the app.js to create sequelize object of the fetched user which can be used in the project globally

--- rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/app.js
+++ rvsn/downloaded resources/2.2 00-starting-setup-frmwrk--from task8---dynamicRoutes1/app.js
@@ -19,6 +19,18 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{   // we are registering this middleware here to store the user in the request with which that user will be made available to use anywhere in this project
+    //  here this middleware will always runs for every incoming request after the server get started inside sequelize as the npm always start the server first for which it has to execute sequelize.sync() first, so by the time this middleware get executed there will always be user available in the user table
+    User.findByPk(1)
+    .then((user)=>{
+        req.user=user;    //  here we are creating a sequelize object of user instance just like req.body and req.params through which it gives access to use user table data anywhere in the project by calling the table field with this(req.user) object
+        next();   // we have to call next() so that other code can we executed once the execution context finish creating the object inside this middleware
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -30,8 +42,17 @@ User.hasMany(Product);   // .hasMany() method works the same way as .belongsT() 
         //                here we have passed an object in .belongsTo() where we define rules for the association in which "constraints" is set true for the authenticity purpose which means the userId which is get added in the product table must be there in the user table and if unAvailable userId detected then database throws err and "onDelete" is set for -> when any user got deleted from the user table then the product containign the user's userId will also get deleted automatically
 
 
-sequelize.sync({alter:true})       //  here when we use sequelize library with node.js then for creating table or for executing models where databse table is created, we have to run sequelize.sync inside app.js and also starts the server in then() as we know once we get the promise response of database creation or availability then only we can handle any data else there is now use of backend program
-                                    // here we use '{alter:true}' for updating the changes which is to be done in the model in the schema which is to be made after once the table is created 
+sequelize.sync()       //  here when we use sequelize library with node.js then for creating table or for executing models where databse table is created, we have to run sequelize.sync inside app.js and also starts the server in then() as we know once we get the promise response of database creation or availability then only we can handle any data else there is now use of backend program
+// sequelize.sync({alter:true})        // here we use '{alter:true}' for updating the changes which is to be done in the model in the schema which is to be made after once the table is created 
+.then((res)=>{   // as we are not have any add User route yet so here we are adding dummy user for testing and execution of functionalities implimented after association of user and product model
+    return User.findByPk(1);   // it checks in the user table that whether anu user with the passed id is available in the table and return the recieved promise response and then it will be handled in nect chained .then()
+})
+.then((user)=>{
+    if(!user){   // this if block will run if the user with the passed id is not available in the table and so here new user will get created 
+        return User.create({name: "Ritikesh",email:"ritikeshjee@gmail.com"})
+    }
+    return user;   // here if the user is not available and the if condition runs then new user will be created and returned and if user available then the user recieved in the response will be returned as it is and then the returned promise response will be handled in next chained .then()
+})
 .then((res)=>{
     // console.log(res);
     app.listen(3000);   // we have added in then() of sync bcz we want to start the server once it get confirmed that the database is created and avaialable to handle data
