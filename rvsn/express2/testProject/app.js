12817256c4eb1819a868bc90cdbb34f15713c586
techy-ritik <ritikeshjee@gmail.com>
2026-07-31 06:35:41 +0530

    unknown routes created and tested

--- rvsn/express2/testProject/app.js
+++ rvsn/express2/testProject/app.js
@@ -11,18 +11,26 @@ app.get("/welcome", addUser, (req, res) => {
   res.send(`<h1>Welcome, ${req.user}!</h1>`);
 });
 
-app.get("/orders", (req, res) => {
-  res.send("Here is the list of all orders.");
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
 });
 
-app.post("/orders", (req, res) => {
-  res.send("A new order has been created.");
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
 });
 
-app.get("/users", (req, res) => {
-  res.send("Here is the list of all users.");
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
 });
