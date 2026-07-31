3879ea5e5223578461acc889c09bcb1d69e80f06
techy-ritik <ritikeshjee@gmail.com>
2026-07-31 18:59:48 +0530

    middleware logging of method and endpoint created and tested

--- rvsn/express2/testProject/app.js
+++ rvsn/express2/testProject/app.js
@@ -11,13 +11,25 @@ app.get("/welcome",addUser,(req, res) => {
   res.send(`<h1>Welcome, ${req.user}!</h1>`);
 });
 
-app.get("/welcome/:username",(req, res)=>{
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next();
+});
 
-  const username = req.params.username;
-  const role = req.query.role;
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
 
-  res.send(`Welcome ${username}, your role is of ${role}`);
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
+});
 
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
 });
 
 app.listen(4000, () => {
