b808393da6e2399725a8c583eba9b41786e4dad0
techy-ritik <ritikeshjee@gmail.com>
2026-08-01 06:25:09 +0530

    route file added and routes separated from app.js

--- rvsn/express2/testProject/app.js
+++ rvsn/express2/testProject/app.js
@@ -2,35 +2,15 @@ const express = require('express');
 
 const app = express();
 
-const addUser = (req, res, next) => {
-  req.user = "Guest";
-  next();
-};
+const route1 = require('./routes/route1')
 
-app.get("/welcome",addUser,(req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
-});
 
 app.use((req, res, next) => {
   console.log(`${req.method} request made to ${req.url}`);
   next();
 });
 
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
-
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
+app.use(route1);
 
 app.listen(4000, () => {
   console.log("Server is running on http://localhost:4000");
