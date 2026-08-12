0bab137b9f469f5482fc0de86613224628f0536c
techy-ritik <ritikeshjee@gmail.com>
2026-08-12 07:13:47 +0530

    crud operations impliemented using sequelize

--- rvsn/express2/studentManagement_system/controllers/student.js
+++ rvsn/express2/studentManagement_system/controllers/student.js
@@ -2,98 +2,90 @@ const db = require("../util/database");
 const studentModel = require('../models/student')
 
 exports.addStudent = (req, res, next) => {
-  // console.log("req.body:", req.body);
+  console.log("req.body:", req.body);
 
-  // const { name, email, age } = req.body;
+  const studentObj = {
+    name:req.body.name,
+    email: req.body.email,
+    age:req.body.age 
+  } 
 
-  // const insertQuery =
-  //   "INSERT INTO students (name, email, age) VALUES (?, ?, ?)";
+    studentModel.create(studentObj)
+    .then((result) => {
+      console.log(result);
 
-  // db.execute(insertQuery, [name, email, age])
-  //   .then(([result]) => {
-  //     console.log(result);
-
-  //     res.status(201).send(`student ${name} added`);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     res.status(500).send(err.message);
-  //   });
+      res.status(201).send("student added");
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    });
 };
 
 exports.fetchAllStudents = (req, res, next) => {
-  // const selectQuery = "SELECT * FROM students";
 
-  // db.execute(selectQuery)
-  //   .then(([result]) => {
-  //       console.log(result)
-  //     res.status(200).json(result);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     res.status(500).send(err.message);
-  //   });
+  studentModel.findAll()
+    .then((result) => {
+        console.log(result)
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    });
 };
 
 exports.fetchStudentById = (req, res, next) => {
-  // const { id } = req.params;
+  const id = req.params.id;
 
-  // const selectQuery = "SELECT * FROM students WHERE id = ?";
-
-  // db.execute(selectQuery, [id])
-  //   .then(([result]) => {
-  //     if (result.length === 0) {
-  //       return res.status(404).send("student not found");
-  //     }
-
-  //     res.status(200).json(result[0]);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     res.status(500).send(err.message);
-  //   });
+    studentModel.findByPk(id)
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    });
 };
 
 exports.updateStudent = (req, res, next) => {
-  // const { id } = req.params;
-  // const { name, email, age } = req.body;
+  const id  = req.params.id;
+  const updatedStudentObj = {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+  }; 
 
-  // const updateQuery =
-  //   "UPDATE students SET name = ?, email = ?, age = ? WHERE id = ?";
 
-  // db.execute(updateQuery, [name, email, age, id])
-  //   .then(([result]) => {
-  //     if (result.affectedRows === 0) {
-  //       return res.status(404).send("student not found");
-  //     }
+  studentModel.update(updatedStudentObj,{where:{id:id}})
+    .then((result) => {
+      console.log(result);
 
-  //     console.log(`Student with ID ${id} updated`);
+      console.log(`Student with ID ${id} updated`);
 
-  //     res.status(200).send("student has been updated");
-  //   })
-  //   .catch((err) => {
-  //     console.log("UPDATE ERROR:", err.message);
-  //     res.status(500).send(err.message);
-  //   });
+      res.status(200).send("student has been updated");
+    })
+    .catch((err) => {
+      console.log("UPDATE ERROR:", err.message);
+      res.status(500).send(err.message);
+    });
 };
 
 exports.deleteStudent = (req, res, next) => {
-  // const { id } = req.params;
+  const  id  = req.params.id;
 
-  // const deleteQuery = "DELETE FROM students WHERE id = ?";
+  studentModel.findByPk(id)
+    .then((student) => {
+      return student.destroy();
+    })
+    .then((result)=>{
+      console.log(`Student with ID ${id} deleted`);
 
-  // db.execute(deleteQuery, [id])
-  //   .then(([result]) => {
-  //     if (result.affectedRows === 0) {
-  //       return res.status(404).send("student not found");
-  //     }
-
-  //     console.log(`Student with ID ${id} deleted`);
-
-  //     res.status(200).send(`student with id ${id} is deleted`);
-  //   })
-  //   .catch((err) => {
-  //     console.log("DELETE ERROR:", err.message);
-  //     res.status(500).send(err.message);
-  //   });
+      res.status(200).send(`student with id ${id} is deleted`);
+    })
+    .catch((err) => {
+      console.log("DELETE ERROR:", err.message);
+      res.status(500).send(err.message);
+    });
 };
