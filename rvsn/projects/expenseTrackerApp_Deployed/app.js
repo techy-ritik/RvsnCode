3f5e1a194b82bb4f86235ac46ad57f47a9135eb7
techy-ritik <ritikeshjee@gmail.com>
2026-08-20 18:13:05 +0530

    expense tracker deployable app project initiated - signup page created

---
+++ rvsn/projects/expenseTrackerApp_Deployed/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json({extended:false}))
+
+const expenseRoutes = require("./routes/expense");
+app.use(expenseRoutes);
+
+const expenseModel = require("./models/expense");
+
+
+const sequelize = require("./util/database");
+
+// sequelize.sync({alter:true})
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(5000,()=>{
+      console.log('server is running on port : 5000')
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ rvsn/projects/expenseTrackerApp_Deployed/util/path.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+const Path = path.dirname(require.main.filename);
+
+module.exports = Path;
